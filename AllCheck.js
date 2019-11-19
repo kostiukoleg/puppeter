ae968183bfb1f53dd3c49a3e20ee20c6dc7c7834
kostiukoleg <olegkostiuk@ukr.net>
2019-11-19 14:02:31 +0200

    Update AllCheck.js

--- AllCheck.js
+++ AllCheck.js
@@ -1,4 +1,5 @@
 function allCheck() {
+	return alert('Working');
     const F = document.forms[0];
     for (let i = 0;i< F.elements.length; i++){
         if((F.elements[i].name.indexOf("downl") == 0 ) && (F.elements[i].type.toLowerCase() == "checkbox")){
