26387f38138dd440795a262022ed0bc24b60ded0
kostiukoleg <olegkostiuk@ukr.net>
2019-11-19 14:03:41 +0200

    Update AllCheck.js

--- AllCheck.js
+++ AllCheck.js
@@ -1,15 +1,7 @@
-function allCheck() {
-	return alert('Working');
-    const F = document.forms[0];
+alert('Working');
+const F = document.forms[0];
     for (let i = 0;i< F.elements.length; i++){
         if((F.elements[i].name.indexOf("downl") == 0 ) && (F.elements[i].type.toLowerCase() == "checkbox")){
            return F.elements[i].checked = true;
         }
     }
-}
-var href = document.location.href;
-
-// Google 
-if (!(/\.(js|css|xml|rss|pdf)$/.test(href)) && ( /^http[s]?:\/\/[^.]*\.(worktool.vifp)\.[a-z\.]+\//.test(href) )) {
-	allCheck();
-}
